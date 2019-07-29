7ced199a85e753df6202816c8c8e61f0a338e4db
ArunaRanjitha <51354222+ArunaRanjitha@users.noreply.github.com>
2019-07-29 10:51:01 +0530

    Update set2_4.py

--- set2_4.py
+++ set2_4.py
@@ -1,11 +1,11 @@
-x,y=map(int,input().split())
-a=list(map(int,input().split()[:x]))
-b=[]
-for i in range(y):
+a,b=map(int,input().split())
+p=list(map(int,input().split()[:a]))
+q=[]
+for i in range(b):
   i=list(map(int,input().split()))
-  b.append(i)
-for i in b:
-  c = a[i[0]-1:i[1]]
+  q.append(i)
+for i in q:
+  c = p[i[0]-1:i[1]]
   d = c[0]
   for j in range(len(c) - 1):
     d = d ^ c[j + 1]
